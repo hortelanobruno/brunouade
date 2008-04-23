1c65f0f13f7c410f127d31ac4a9055ab4509ff6a
hortelanobruno <hortelanobruno@bbc01b02-664b-0410-8048-2f08f105149e>
2008-04-23 23:58:09 +0000

    Initial import.

---
+++ ZaraCentroDeDistribucionServer/src/src/ServerFacadeBean.java
@@ -0,0 +1,16 @@
+package src;
+
+import javax.ejb.Stateless;
+import BusinessLogic.ServerFacade;
+import BusinessLogic.SolicitudDistribucion;
+
+@Stateless
+public class ServerFacadeBean implements ServerFacade 
+{
+
+	public void guardarSolicitud(SolicitudDistribucion soldist) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
