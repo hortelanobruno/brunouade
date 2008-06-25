b05eef58fdfcb64bccd96fa31058cef04c793c6d
hortelanobruno <hortelanobruno@bbc01b02-664b-0410-8048-2f08f105149e>
2008-06-25 23:39:58 +0000

    Revision 1

---
+++ branches/ZaraServerRev1/src/BusinessLogic/SolicitudDeFabricacion.java
@@ -0,0 +1,181 @@
+package BusinessLogic;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Locale;
+import java.util.Vector;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+
+import VO.ArticuloAFabricarVO;
+import VO.SolicitudFabricaVO;
+
+@Entity
+@DiscriminatorValue("fabricacion")
+public class SolicitudDeFabricacion extends Solicitud
+{
+	private static final long serialVersionUID = 4970004249380972083L;
+	private Fabrica fabrica;
+	private Collection<ArticuloAFabricar> articulosAFabricar;
+	private int idFab;
+	private boolean cerrada;
+	
+	public SolicitudDeFabricacion() {
+		super();
+		this.articulosAFabricar = new Vector<ArticuloAFabricar>();
+	}
+	
+	public SolicitudDeFabricacion(int id,int n, Collection<ArticuloAFabricar> a, Date f, Fabrica fa)
+	{
+		super(id,f);
+		this.cerrada = false;
+		this.idFab = n;
+		this.fabrica = fa;
+		this.setArticulosAFabricar(a);
+	}
+	
+	@Column
+	public boolean getCerrada() {
+		return cerrada;
+	}
+
+	public void setCerrada(boolean cerrada) {
+		this.cerrada = cerrada;
+	}
+	
+	@Column
+	public int getIdFab() {
+		return idFab;
+	}
+
+	public void setIdFab(int idFab) {
+		this.idFab = idFab;
+	}
+	
+	@ManyToOne
+	public Fabrica getFabrica() {
+		return fabrica;
+	}
+
+	public void setFabrica(Fabrica fabrica) {
+		this.fabrica = fabrica;
+	}
+
+	@OneToMany(cascade={CascadeType.MERGE})
+	public Collection<ArticuloAFabricar> getArticulosAFabricar() {
+		return articulosAFabricar;
+	}
+
+	public void setArticulosAFabricar(Collection<ArticuloAFabricar> articulosAFabricar) {
+		this.articulosAFabricar = articulosAFabricar;
+	}
+	
+	@Transient
+	public SolicitudFabricaVO getVO()
+	{
+		SolicitudFabricaVO vo = new SolicitudFabricaVO();
+		vo.setIdFab(this.getIdFab());
+		vo.setFabrica(this.getFabrica().getVO());
+		vo.setFechaEmision(this.getFechaHoraFromString(this.getFechaEmision()));
+		vo.setCdVO(this.getCentro().getVO());
+		vo.setId(this.getId());
+		vo.setCerrada(this.getCerrada());
+		Iterator it = (Iterator) this.getArticulosAFabricar().iterator();
+		Collection<ArticuloAFabricarVO> arts = new ArrayList<ArticuloAFabricarVO>();
+		while(it.hasNext()){
+			ArticuloAFabricar art = ((ArticuloAFabricar)it.next());
+			arts.add(art.getVO());
+		}
+		vo.setArticulosAFabricar(arts);
+		return vo;
+	}
+
+	public void setVO(SolicitudFabricaVO vo)
+	{
+		Collection<ArticuloAFabricar> recibidos = new ArrayList<ArticuloAFabricar>();
+		Iterator rec = (Iterator) vo.getArticulosAFabricar().iterator();
+		while(rec.hasNext()){
+			ArticuloAFabricarVO art = (ArticuloAFabricarVO)rec.next();
+			ArticuloAFabricar arti = new ArticuloAFabricar();
+			arti.setVO(art);
+			recibidos.add(arti);
+		}
+		CentroDistribucion cent = new CentroDistribucion();
+		cent.setVO(vo.getCdVO());
+		this.setCentro(cent);
+		this.setIdFab(vo.getIdFab());
+		this.setFechaEmision(this.getFechaHoraFromDate(vo.getFechaEmision()));
+		Fabrica fab = new Fabrica();
+		fab.setVO(vo.getFabrica());
+		this.setFabrica(fab);
+		this.setId(vo.getId());
+		this.setArticulosAFabricar(recibidos);
+		this.setCerrada(vo.getCerrada());
+	}
+	
+	@Transient
+	private String getFecha(String f)
+	{
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i<f.indexOf(" ");i++)
+			sb.append(f.charAt(i));
+		
+		return sb.toString();
+	}
+	
+	@Transient
+	private String getHora(String f)
+	{
+		StringBuffer sb = new StringBuffer();
+		for(int i = f.indexOf(" "); i<f.length();i++)
+			sb.append(f.charAt(i));
+		
+		return sb.toString();
+	}
+	
+	@Transient
+	private String getFechaHoraFromDate(Date d)
+	{
+		String fecha;
+		DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault());
+		DateFormat df1 = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault());
+		fecha = df.format(d ) + " " +df1.format(d );
+		return fecha;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Transient
+	private Date getFechaHoraFromString(String f)
+	{
+		DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault());
+		DateFormat df1 = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault());
+		
+		Date fn;
+		Date fn2;
+		try 
+		{
+			fn = df.parse(this.getFecha(f));
+			fn2 = df1.parse(this.getHora(f));	
+			fn.setHours(fn2.getHours());
+			fn.setMinutes(fn2.getMinutes());
+			fn.setSeconds(fn2.getSeconds());
+		} 
+		catch (ParseException e)
+		{
+			// TODO Auto-generated catch block
+			return null;
+		}
+		return fn;
+	}
+
+}
---
+++ branches/ZaraServerRev1/src/BusinessLogic/SolicitudDistribucion.java
@@ -0,0 +1,177 @@
+package BusinessLogic;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Locale;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
+
+import VO.ArticuloPedidoVO;
+import VO.SolicitudDistribucionVO;
+
+@Entity
+@DiscriminatorValue("distribucion")
+public class SolicitudDistribucion extends Solicitud
+{
+	private static final long serialVersionUID = 1694955054674640622L;
+	private Tienda tienda;
+	private Collection<ArticuloPedido> articulosPedidos;
+	private boolean cerrada;
+	private int idDis;
+
+
+	public SolicitudDistribucion() {
+		super();
+		this.setCerrada(false);
+	}
+
+	public SolicitudDistribucion(int id,int n, Date f, Tienda t, Collection<ArticuloPedido> a) {
+		super(id,f);
+		this.idDis = n;
+		this.tienda = t;
+		this.setArticulosPedidos(a);
+		this.setCerrada(false);
+	}
+
+
+	@Column
+	public int getIdDis() {
+		return idDis;
+	}
+
+	public void setIdDis(int idDis) {
+		this.idDis = idDis;
+	}
+	
+	@ManyToOne(cascade={CascadeType.MERGE})
+	public Tienda getTienda() {
+		return tienda;
+	}
+
+	public void setTienda(Tienda tienda) {
+		this.tienda = tienda;
+	}
+
+	@OneToMany(cascade={CascadeType.MERGE})
+	public Collection<ArticuloPedido> getArticulosPedidos() {
+		return articulosPedidos;
+	}
+
+	public void setArticulosPedidos(Collection<ArticuloPedido> articulosPedidos) {
+		this.articulosPedidos = articulosPedidos;
+	}
+	
+	@Column
+	public boolean getCerrada() {
+		return cerrada;
+	}
+
+	public void setCerrada(boolean cerrada) {
+		this.cerrada = cerrada;
+	}
+
+	@Transient
+	public SolicitudDistribucionVO getVO() {
+		SolicitudDistribucionVO sol = new SolicitudDistribucionVO();
+		sol.setIdDis(this.getIdDis());
+		sol.setFechaEmision(this.getFechaHoraFromString(this.getFechaEmision()));
+		sol.setTienda(this.getTienda().getVO());
+		sol.setCdVO(this.getCentro().getVO());
+		sol.setId(this.getId());
+		Collection<ArticuloPedidoVO> arts = new ArrayList<ArticuloPedidoVO>();
+		Iterator it = (Iterator) this.getArticulosPedidos().iterator();
+		while(it.hasNext()){
+			ArticuloPedidoVO art = ((ArticuloPedido)it.next()).getVO();
+			arts.add(art);
+		}
+		sol.setArticulosPedidos(arts);
+		return sol;
+	}
+
+	public void setVO(SolicitudDistribucionVO vo) {
+		this.setFechaEmision(this.getFechaHoraFromDate(vo.getFechaEmision()));
+		this.setIdDis(vo.getIdDis());
+		Tienda tienda = new Tienda();
+		tienda.setVO(vo.getTienda());
+		this.setTienda(tienda);
+		CentroDistribucion centro = new CentroDistribucion();
+		centro.setVO(vo.getCdVO());
+		this.setCentro(centro);
+		this.setCerrada(vo.getCerrada());
+		Collection<ArticuloPedido> arts = new ArrayList<ArticuloPedido>();
+		Iterator it = (Iterator) vo.getArticulosPedidos().iterator();
+		while(it.hasNext()){
+			ArticuloPedido art = new ArticuloPedido();
+			ArticuloPedidoVO artVO = ((ArticuloPedidoVO)it.next());
+			art.setVO(artVO);
+			arts.add(art);
+		}
+		this.setArticulosPedidos(arts);
+		this.setId(vo.getId());
+	}
+	
+	@Transient
+	private String getFecha(String f)
+	{
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i<f.indexOf(" ");i++)
+			sb.append(f.charAt(i));
+		
+		return sb.toString();
+	}
+	
+	@Transient
+	private String getHora(String f)
+	{
+		StringBuffer sb = new StringBuffer();
+		for(int i = f.indexOf(" "); i<f.length();i++)
+			sb.append(f.charAt(i));
+		
+		return sb.toString();
+	}
+	
+	@Transient
+	private String getFechaHoraFromDate(Date d)
+	{
+		String fecha;
+		DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault());
+		DateFormat df1 = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault());
+		fecha = df.format(d ) + " " +df1.format(d );
+		return fecha;
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Transient
+	private Date getFechaHoraFromString(String f)
+	{
+		DateFormat df = DateFormat.getDateInstance(DateFormat.SHORT, Locale.getDefault());
+		DateFormat df1 = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault());
+		
+		Date fn;
+		Date fn2;
+		try 
+		{
+			fn = df.parse(this.getFecha(f));
+			fn2 = df1.parse(this.getHora(f));	
+			fn.setHours(fn2.getHours());
+			fn.setMinutes(fn2.getMinutes());
+			fn.setSeconds(fn2.getSeconds());
+		} 
+		catch (ParseException e)
+		{
+			// TODO Auto-generated catch block
+			return null;
+		}
+		return fn;
+	}
+}
