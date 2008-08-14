0a8251b38d84df84ea673814bb18859075c3694b
hortelanobruno <hortelanobruno@bbc01b02-664b-0410-8048-2f08f105149e>
2008-08-14 18:54:23 +0000

    Initial import.

---
+++ ZaraCentroDeDistribucionServerWeb/src/beans/AdministradorArticulos.java
@@ -0,0 +1,45 @@
+package beans;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.ejb.Local;
+
+import Exceptions.ExistingProductException;
+import VO.ArticuloAEnviarVO;
+import VO.ArticuloAFabricarVO;
+import VO.ArticuloAReponerVO;
+import VO.ArticuloHeaderVO;
+import VO.ArticuloHogarVO;
+import VO.ArticuloReservadoVO;
+import VO.ArticuloRopaVO;
+
+
+@Local
+public interface AdministradorArticulos 
+{
+
+	public int getNextIdArticuloPedido();
+	public int getNextIdArticuloAEnviar();
+	public int getNextIdArticuloAFabricar();
+	public int getNextIdArticuloAReponer();
+	public int getNextIdArticuloReservado();
+	public void guardarArticuloHogar(ArticuloHogarVO a) throws ExistingProductException;
+	public void guardarArticuloRopa(ArticuloRopaVO a) throws ExistingProductException;
+	public ArrayList<String> getDescripciones(ArrayList<Long> cods);
+	public ArrayList<Integer> getStocks(ArrayList<Long> codigos);
+	public void actualizarStock(ArrayList<ArticuloAReponerVO> arts);
+	public ArticuloHeaderVO getArticulo(long codigo);
+	public void guardarArticulosAFabricar(Collection<ArticuloAFabricarVO> artic2);
+	public void guardarArticulosAEnviar(Collection<ArticuloAEnviarVO> artic2);
+	public void modificarStock(Collection<ArticuloReservadoVO> artiAEnv);
+	public ArrayList<ArticuloAFabricarVO> getArticulosAFabricar();
+	public boolean existeArticulo(long codigo);
+	public void guardarArticulosReservados(Collection<ArticuloReservadoVO> artiReser);
+	public ArrayList<ArticuloHeaderVO> getArticulos(ArrayList<Long> codigos);
+	public ArrayList<ArticuloReservadoVO> getArtsReservados(int codSolDis);
+	public void actArtsRes(ArrayList<ArticuloReservadoVO> articulosReservados);
+	public void actualizarStock(ArrayList<ArticuloAEnviarVO> articulosAEnviar, ArrayList<ArticuloReservadoVO> articulosReservados);
+	public ArrayList<ArticuloAEnviarVO> getArtsAEnv(int codSolDis);
+	public ArrayList<Long> existenArts(ArrayList<Long> codigos);
+}
