748d47b35a2b025d07a1c414a2bcdcff36d741e1
ignaciocicero <ignaciocicero@bbc01b02-664b-0410-8048-2f08f105149e>
2008-11-12 18:52:39 +0000

    Initial import.

---
+++ SimuladorServerFabrica/src/webServices/RecibirSolFabrSoapBindingImpl.java
@@ -0,0 +1,16 @@
+/**
+ * RecibirSolFabrSoapBindingImpl.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.3 Oct 05, 2005 (05:23:37 EDT) WSDL2Java emitter.
+ */
+
+package webServices;
+
+public class RecibirSolFabrSoapBindingImpl implements webServices.RecibirSolFabr{
+    public boolean recibirSolFabr(java.lang.String in0) throws java.rmi.RemoteException {
+    	System.out.println("llego!");
+        return false;
+    }
+
+}
