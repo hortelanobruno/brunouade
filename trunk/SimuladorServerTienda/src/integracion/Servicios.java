668816202a9f70ee4e65f64f62019d2137a26377
ignaciocicero <ignaciocicero@bbc01b02-664b-0410-8048-2f08f105149e>
2008-11-15 15:07:42 +0000

    Initial import.

---
+++ trunk/SimuladorServerTienda/src/integracion/Servicios.java
@@ -0,0 +1,13 @@
+/**
+ * Servicios.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.3 Oct 05, 2005 (05:23:37 EDT) WSDL2Java emitter.
+ */
+
+package integracion;
+
+public interface Servicios extends java.rmi.Remote {
+    public boolean recibirEnvio(java.lang.String in0) throws java.rmi.RemoteException;
+    public boolean recibirOfad(java.lang.String in0) throws java.rmi.RemoteException;
+}
---
+++ trunk/SimuladorServerTienda/src/unregisterService.bat
@@ -0,0 +1,10 @@
+@echo on
+set axis_home=d:\axis-1_3
+
+set NAME=Servicios
+set PACKAGE=integracion
+
+set cp=%axis_home%\lib\axis.jar;%axis_home%\lib\axis-ant.jar;%axis_home%\lib\commons-discovery-0.2.jar;%axis_home%\lib\commons-logging-1.0.4.jar;%axis_home%\lib\jaxrpc.jar;%axis_home%\lib\log4j-1.2.8.jar;%axis_home%\lib\saaj.jar;%axis_home%\lib\wsdl4j-1.5.1.jar
+set cp=%cp%;.\bin;%CLASSPATH%;
+
+java -classpath %cp% org.apache.axis.client.AdminClient %PACKAGE%\undeploy.wsdd
