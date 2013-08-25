b318bf4a1235a8c22c26ce1bd9769f8468403d28
hortelanobruno <hortelanobruno@bbc01b02-664b-0410-8048-2f08f105149e>
2013-08-25 20:43:29 +0000

    Administracion parte de noticias lista

--- TFC3/bonfire/application/logs/log-2013-08-25.php
+++ TFC3/bonfire/application/logs/log-2013-08-25.php
@@ -35,3 +35,10 @@ ERROR - 2013-08-25 21:53:49 --> Severity: Warning  --> mkdir() [<a href='functio
 ERROR - 2013-08-25 22:04:27 --> Query error: Incorrect integer value: '' for column 'cantidad_partidos_perdidos' at row 1
 ERROR - 2013-08-25 22:05:02 --> Query error: Incorrect integer value: '' for column 'cantidad_partidos_perdidos' at row 1
 ERROR - 2013-08-25 22:09:40 --> Query error: Incorrect integer value: '' for column 'cantidad_tarjetas_rojas' at row 1
+ERROR - 2013-08-25 22:19:21 --> Severity: Warning  --> mkdir() [<a href='function.mkdir'>function.mkdir</a>]: File exists C:\Users\Brunoli\Documents\NetBeansProjects\TFC3\bonfire\application\core_modules\builder\libraries\modulebuilder.php 315
+ERROR - 2013-08-25 22:19:22 --> Severity: Warning  --> mkdir() [<a href='function.mkdir'>function.mkdir</a>]: File exists C:\Users\Brunoli\Documents\NetBeansProjects\TFC3\bonfire\application\core_modules\builder\libraries\modulebuilder.php 315
+ERROR - 2013-08-25 22:19:22 --> Severity: Warning  --> mkdir() [<a href='function.mkdir'>function.mkdir</a>]: File exists C:\Users\Brunoli\Documents\NetBeansProjects\TFC3\bonfire\application\core_modules\builder\libraries\modulebuilder.php 315
+ERROR - 2013-08-25 22:19:22 --> Severity: Warning  --> mkdir() [<a href='function.mkdir'>function.mkdir</a>]: File exists C:\Users\Brunoli\Documents\NetBeansProjects\TFC3\bonfire\application\core_modules\builder\libraries\modulebuilder.php 315
+ERROR - 2013-08-25 22:25:10 --> Query error: Incorrect integer value: '' for column 'idequipo' at row 1
+ERROR - 2013-08-25 22:25:42 --> Query error: Incorrect integer value: '' for column 'cantidad_tarjetas_amarillas' at row 1
+ERROR - 2013-08-25 22:29:52 --> Query error: Unknown table 'eq'
---
+++ TFC3/bonfire/modules/jugadores/controllers/content.php
@@ -0,0 +1,205 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class content extends Admin_Controller {
+
+    //--------------------------------------------------------------------
+
+
+    public function __construct() {
+        parent::__construct();
+
+        $this->auth->restrict('Jugadores.Content.View');
+        $this->load->model('jugadores_model', null, true);
+        $this->load->model('equipos_model', null, true);
+        $this->lang->load('jugadores');
+
+        Template::set_block('sub_nav', 'content/_sub_nav');
+    }
+
+    //--------------------------------------------------------------------
+
+
+
+    /*
+      Method: index()
+
+      Displays a list of form data.
+     */
+    public function index() {
+
+        // Deleting anything?
+        if ($this->input->post_key_exists('delete')) {
+            $checked = $this->input->post('checked');
+
+            if (is_array($checked) && count($checked)) {
+                $result = FALSE;
+                foreach ($checked as $pid) {
+                    $result = $this->jugadores_model->delete($pid);
+                }
+
+                if ($result) {
+                    Template::set_message(count($checked) . ' ' . lang('jugadores_delete_success'), 'success');
+                } else {
+                    Template::set_message(lang('jugadores_delete_failure') . $this->jugadores_model->error, 'error');
+                }
+            }
+        }
+
+        $records = $this->jugadores_model->find_all();
+
+        Template::set('records', $records);
+        Template::set('equipos', $this->equipos_model->get_all_equipos());
+        Template::set('toolbar_title', 'Manage Jugadores');
+        Template::render();
+    }
+
+    //--------------------------------------------------------------------
+
+
+
+    /*
+      Method: create()
+
+      Creates a Jugadores object.
+     */
+    public function create() {
+        $this->auth->restrict('Jugadores.Content.Create');
+
+        if ($this->input->post_key_exists('save')) {
+            if ($insert_id = $this->save_jugadores()) {
+                // Log the activity
+                $this->activity_model->log_activity($this->current_user->id, lang('jugadores_act_create_record') . ': ' . $insert_id . ' : ' . $this->input->ip_address(), 'jugadores');
+
+                Template::set_message(lang('jugadores_create_success'), 'success');
+                redirect(SITE_AREA . '/content/jugadores');
+            } else {
+                Template::set_message(lang('jugadores_create_failure') . $this->jugadores_model->error, 'error');
+            }
+        }
+        Assets::add_module_js('jugadores', 'jugadores.js');
+
+        Template::set('equipos', $this->equipos_model->get_all_equipos());
+        Template::set('toolbar_title', lang('jugadores_create') . ' Jugadores');
+        Template::render();
+    }
+
+    //--------------------------------------------------------------------
+
+
+
+    /*
+      Method: edit()
+
+      Allows editing of Jugadores data.
+     */
+    public function edit() {
+        $id = $this->uri->segment(5);
+
+        if (empty($id)) {
+            Template::set_message(lang('jugadores_invalid_id'), 'error');
+            redirect(SITE_AREA . '/content/jugadores');
+        }
+
+        if ($this->input->post_key_exists('save')) {
+            $this->auth->restrict('Jugadores.Content.Edit');
+
+            if ($this->save_jugadores('update', $id)) {
+                // Log the activity
+                $this->activity_model->log_activity($this->current_user->id, lang('jugadores_act_edit_record') . ': ' . $id . ' : ' . $this->input->ip_address(), 'jugadores');
+
+                Template::set_message(lang('jugadores_edit_success'), 'success');
+            } else {
+                Template::set_message(lang('jugadores_edit_failure') . $this->jugadores_model->error, 'error');
+            }
+        } else if ($this->input->post_key_exists('delete')) {
+            $this->auth->restrict('Jugadores.Content.Delete');
+
+            if ($this->jugadores_model->delete($id)) {
+                // Log the activity
+                $this->activity_model->log_activity($this->current_user->id, lang('jugadores_act_delete_record') . ': ' . $id . ' : ' . $this->input->ip_address(), 'jugadores');
+
+                Template::set_message(lang('jugadores_delete_success'), 'success');
+
+                redirect(SITE_AREA . '/content/jugadores');
+            } else {
+                Template::set_message(lang('jugadores_delete_failure') . $this->jugadores_model->error, 'error');
+            }
+        }
+        Template::set('jugadores', $this->jugadores_model->find($id));
+        Assets::add_module_js('jugadores', 'jugadores.js');
+        Template::set('equipos', $this->equipos_model->get_all_equipos());
+        Template::set('toolbar_title', lang('jugadores_edit') . ' Jugadores');
+        Template::render();
+    }
+
+    //--------------------------------------------------------------------
+    //--------------------------------------------------------------------
+    // !PRIVATE METHODS
+    //--------------------------------------------------------------------
+
+    /*
+      Method: save_jugadores()
+
+      Does the actual validation and saving of form data.
+
+      Parameters:
+      $type	- Either "insert" or "update"
+      $id		- The ID of the record to update. Not needed for inserts.
+
+      Returns:
+      An INT id for successful inserts. If updating, returns TRUE on success.
+      Otherwise, returns FALSE.
+     */
+    private function save_jugadores($type = 'insert', $id = 0) {
+        if ($type == 'update') {
+            $_POST['id'] = $id;
+        }
+
+
+        $this->form_validation->set_rules('jugadores_nombre_completo', 'Nombre Completo', 'max_length[200]');
+        $this->form_validation->set_rules('jugadores_nombre', 'Nombre', 'max_length[200]');
+        $this->form_validation->set_rules('jugadores_apellido', 'Apellido', 'max_length[200]');
+        $this->form_validation->set_rules('jugadores_delegado', 'Delegado', 'max_length[1]');
+        $this->form_validation->set_rules('jugadores_idequipo', 'Idequipo', 'max_length[11]');
+        $this->form_validation->set_rules('jugadores_cantidad_tarjetas_amarillas', 'Cantidad Tarjetas Amarillas', 'max_length[11]');
+        $this->form_validation->set_rules('jugadores_cantidad_partidos_jugados', 'Cantidad Partidos Jugados', 'max_length[11]');
+        $this->form_validation->set_rules('jugadores_cantidad_tarjetas_rojas', 'Cantidad Tarjetas Rojas', 'max_length[11]');
+        $this->form_validation->set_rules('jugadores_cantidad_goles', 'Cantidad Goles', 'max_length[11]');
+
+        if ($this->form_validation->run() === FALSE) {
+            return FALSE;
+        }
+
+        // make sure we only pass in the fields we want
+
+        $data = array();
+        $data['nombre_completo'] = $this->input->post('jugadores_nombre_completo');
+        $data['nombre'] = $this->input->post('jugadores_nombre');
+        $data['apellido'] = $this->input->post('jugadores_apellido');
+        $data['delegado'] = $this->input->post('jugadores_delegado');
+        $data['idequipo'] = intval($this->input->post('jugadores_idequipo'));
+        $data['cantidad_tarjetas_amarillas'] = intval($this->input->post('jugadores_cantidad_tarjetas_amarillas'));
+        $data['cantidad_partidos_jugados'] = intval($this->input->post('jugadores_cantidad_partidos_jugados'));
+        $data['cantidad_tarjetas_rojas'] = intval($this->input->post('jugadores_cantidad_tarjetas_rojas'));
+        $data['cantidad_goles'] = intval($this->input->post('jugadores_cantidad_goles'));
+
+        if ($type == 'insert') {
+            $id = $this->jugadores_model->insert($data);
+
+            if (is_numeric($id)) {
+                $return = $id;
+            } else {
+                $return = FALSE;
+            }
+        } else if ($type == 'update') {
+            $return = $this->jugadores_model->update($id, $data);
+        }
+
+        return $return;
+    }
+
+    //--------------------------------------------------------------------
+}
