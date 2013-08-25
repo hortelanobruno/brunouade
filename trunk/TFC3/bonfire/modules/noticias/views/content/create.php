658d89e1e87622cb167e6460c1cd52b747d8be3d
hortelanobruno <hortelanobruno@bbc01b02-664b-0410-8048-2f08f105149e>
2013-08-25 19:39:34 +0000

    Administracion parte de noticias lista

--- trunk/TFC3/bonfire/modules/noticias/views/content/create.php
+++ trunk/TFC3/bonfire/modules/noticias/views/content/create.php
@@ -1,50 +1,63 @@
 
 <?php if (validation_errors()) : ?>
-<div class="alert alert-block alert-error fade in ">
-  <a class="close" data-dismiss="alert">&times;</a>
-  <h4 class="alert-heading">Please fix the following errors :</h4>
- <?php echo validation_errors(); ?>
-</div>
+    <div class="alert alert-block alert-error fade in ">
+        <a class="close" data-dismiss="alert">&times;</a>
+        <h4 class="alert-heading">Please fix the following errors :</h4>
+        <?php echo validation_errors(); ?>
+    </div>
 <?php endif; ?>
-<?php // Change the css classes to suit your needs
-if( isset($noticias) ) {
-    $noticias = (array)$noticias;
+<?php
+// Change the css classes to suit your needs
+if (isset($noticias)) {
+    $noticias = (array) $noticias;
 }
 $id = isset($noticias['idnoticias']) ? $noticias['idnoticias'] : '';
 ?>
 <div class="admin-box">
     <h3>Noticias</h3>
-<?php echo form_open($this->uri->uri_string(), 'class="form-horizontal"'); ?>
+    <?php echo form_open($this->uri->uri_string(), 'class="form-horizontal"'); ?>
     <fieldset>
         <div class="control-group <?php echo form_error('noticias_idtorneo') ? 'error' : ''; ?>">
-            <?php echo form_label('Idtorneo', 'noticias_idtorneo', array('class' => "control-label") ); ?>
+            <?php echo form_label('Torneo', 'noticias_idtorneo', array('class' => "control-label")); ?>
+            <div class="controls">
+                <!--input id="noticias_idtorneo" type="text" name="noticias_idtorneo" maxlength="11" value="<!?php echo set_value('noticias_idtorneo', isset($noticias['idtorneo']) ? $noticias['idtorneo'] : ''); ?>"  /> -->
+                
+                <?php if (isset($torneos)) { ?>
+                <select id="noticias_idtorneo" name="noticias_idtorneo">
+                    <?php foreach ($torneos as $torneo): ?>
+                        <option value="<?php echo $torneo['id'] ?>"><?php echo $torneo['nombre'] ?></option>
+                    <?php endforeach ?>
+                    <option value="-1">None</option>
+                </select>
+                <?php } ?>
+                
+                <span class="help-inline"><?php echo form_error('noticias_idtorneo'); ?></span>
+            </div> 
+        </div>  
+        
+
+
+        <div class="control-group <?php echo form_error('noticias_titulo') ? 'error' : ''; ?>">
+            <?php echo form_label('Titulo', 'noticias_titulo', array('class' => "control-label")); ?>
             <div class="controls">
 
-               <input id="noticias_idtorneo" type="text" name="noticias_idtorneo" maxlength="11" value="<?php echo set_value('noticias_idtorneo', isset($noticias['idtorneo']) ? $noticias['idtorneo'] : ''); ?>"  />
-               <span class="help-inline"><?php echo form_error('noticias_idtorneo'); ?></span>
-            </div>
-
-        </div>        <div class="control-group <?php echo form_error('noticias_titulo') ? 'error' : ''; ?>">
-            <?php echo form_label('Titulo', 'noticias_titulo', array('class' => "control-label") ); ?>
-            <div class="controls">
-
-               <input id="noticias_titulo" type="text" name="noticias_titulo" maxlength="200" value="<?php echo set_value('noticias_titulo', isset($noticias['titulo']) ? $noticias['titulo'] : ''); ?>"  />
-               <span class="help-inline"><?php echo form_error('titulo'); ?></span>
+                <input id="noticias_titulo" type="text" name="noticias_titulo" maxlength="200" value="<?php echo set_value('noticias_titulo', isset($noticias['titulo']) ? $noticias['titulo'] : ''); ?>"  />
+                <span class="help-inline"><?php echo form_error('titulo'); ?></span>
             </div>
 
         </div>        <div class="control-group <?php echo form_error('noticias_contenido') ? 'error' : ''; ?>">
-            <?php echo form_label('Contenido', 'noticias_contenido', array('class' => "control-label") ); ?>
+            <?php echo form_label('Contenido', 'noticias_contenido', array('class' => "control-label")); ?>
             <div class="controls">
-                <?php echo form_textarea( array( 'name' => 'noticias_contenido', 'id' => 'noticias_contenido', 'rows' => '5', 'cols' => '80', 'value' => set_value('noticias_contenido', isset($noticias['contenido']) ? $noticias['contenido'] : '') ) )?>
+                <?php echo form_textarea(array('name' => 'noticias_contenido', 'id' => 'noticias_contenido', 'rows' => '5', 'cols' => '80', 'value' => set_value('noticias_contenido', isset($noticias['contenido']) ? $noticias['contenido'] : ''))) ?>
                 <span class="help-inline"><?php echo form_error('noticias_contenido'); ?></span>
             </div>
 
         </div>        <div class="control-group <?php echo form_error('noticias_fecha') ? 'error' : ''; ?>">
-            <?php echo form_label('Fecha', 'noticias_fecha', array('class' => "control-label") ); ?>
+            <?php echo form_label('Fecha', 'noticias_fecha', array('class' => "control-label")); ?>
             <div class="controls">
 
-               <input id="noticias_fecha" type="text" name="noticias_fecha" maxlength="200" value="<?php echo set_value('noticias_fecha', isset($noticias['fecha']) ? $noticias['fecha'] : ''); ?>"  />
-               <span class="help-inline"><?php echo form_error('noticias_fecha'); ?></span>
+                <input id="noticias_fecha" type="text" name="noticias_fecha" maxlength="200" value="<?php echo set_value('noticias_fecha', isset($noticias['fecha']) ? $noticias['fecha'] : ''); ?>"  />
+                <span class="help-inline"><?php echo form_error('noticias_fecha'); ?></span>
             </div>
 
         </div>
@@ -53,8 +66,8 @@ $id = isset($noticias['idnoticias']) ? $noticias['idnoticias'] : '';
         <div class="form-actions">
             <br/>
             <input type="submit" name="save" class="btn btn-primary" value="Create Noticias" />
-            or <?php echo anchor(SITE_AREA .'/content/noticias', lang('noticias_cancel'), 'class="btn btn-warning"'); ?>
-            
+            or <?php echo anchor(SITE_AREA . '/content/noticias', lang('noticias_cancel'), 'class="btn btn-warning"'); ?>
+
         </div>
     </fieldset>
     <?php echo form_close(); ?>
--- trunk/TFC3/bonfire/modules/noticias/views/content/index.php
+++ trunk/TFC3/bonfire/modules/noticias/views/content/index.php
@@ -1,56 +1,71 @@
 <div class="admin-box">
-	<h3>Noticias</h3>
-	<?php echo form_open($this->uri->uri_string()); ?>
-		<table class="table table-striped">
-			<thead>
-				<tr>
-					<?php if ($this->auth->has_permission('Noticias.Content.Delete') && isset($records) && is_array($records) && count($records)) : ?>
-					<th class="column-check"><input class="check-all" type="checkbox" /></th>
-					<?php endif;?>
-					
-					<th>Idtorneo</th>
-					<th>Titulo</th>
-					<th>Contenido</th>
-					<th>Fecha</th>
-				</tr>
-			</thead>
-			<?php if (isset($records) && is_array($records) && count($records)) : ?>
-			<tfoot>
-				<?php if ($this->auth->has_permission('Noticias.Content.Delete')) : ?>
-				<tr>
-					<td colspan="5">
-						<?php echo lang('bf_with_selected') ?>
-						<input type="submit" name="delete" id="delete-me" class="btn btn-danger" value="<?php echo lang('bf_action_delete') ?>" onclick="return confirm('<?php e(js_escape(lang('noticias_delete_confirm'))); ?>')">
-					</td>
-				</tr>
-				<?php endif;?>
-			</tfoot>
-			<?php endif; ?>
-			<tbody>
-			<?php if (isset($records) && is_array($records) && count($records)) : ?>
-			<?php foreach ($records as $record) : ?>
-				<tr>
-					<?php if ($this->auth->has_permission('Noticias.Content.Delete')) : ?>
-					<td><input type="checkbox" name="checked[]" value="<?php echo $record->idnoticias ?>" /></td>
-					<?php endif;?>
-					
-				<?php if ($this->auth->has_permission('Noticias.Content.Edit')) : ?>
-				<td><?php echo anchor(SITE_AREA .'/content/noticias/edit/'. $record->idnoticias, '<i class="icon-pencil">&nbsp;</i>' .  $record->idtorneo) ?></td>
-				<?php else: ?>
-				<td><?php e($record->idtorneo) ?></td>
-				<?php endif; ?>
-			
-				<td><?php e($record->titulo) ?></td>
-				<td><?php //e($record->contenido) ?></td>
-				<td><?php e($record->fecha) ?></td>
-				</tr>
-			<?php endforeach; ?>
-			<?php else: ?>
-				<tr>
-					<td colspan="5">No records found that match your selection.</td>
-				</tr>
-			<?php endif; ?>
-			</tbody>
-		</table>
-	<?php echo form_close(); ?>
+    <h3>Noticias</h3>
+    <?php echo form_open($this->uri->uri_string()); ?>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <?php if ($this->auth->has_permission('Noticias.Content.Delete') && isset($records) && is_array($records) && count($records)) : ?>
+                    <th class="column-check"><input class="check-all" type="checkbox" /></th>
+                <?php endif; ?>
+
+                <th>Torneo</th>
+                <th>Titulo</th>
+                <th>Contenido</th>
+                <th>Fecha</th>
+            </tr>
+        </thead>
+        <?php if (isset($records) && is_array($records) && count($records)) : ?>
+            <tfoot>
+                <?php if ($this->auth->has_permission('Noticias.Content.Delete')) : ?>
+                    <tr>
+                        <td colspan="5">
+                            <?php echo lang('bf_with_selected') ?>
+                            <input type="submit" name="delete" id="delete-me" class="btn btn-danger" value="<?php echo lang('bf_action_delete') ?>" onclick="return confirm('<?php e(js_escape(lang('noticias_delete_confirm'))); ?>')">
+                        </td>
+                    </tr>
+                <?php endif; ?>
+            </tfoot>
+        <?php endif; ?>
+        <tbody>
+            <?php if (isset($records) && is_array($records) && count($records)) : ?>
+                <?php foreach ($records as $record) : ?>
+                    <tr>
+                        <?php if ($this->auth->has_permission('Noticias.Content.Delete')) : ?>
+                            <td><input type="checkbox" name="checked[]" value="<?php echo $record->idnoticias ?>" /></td>
+                        <?php endif; ?>
+
+                        <?php if ($this->auth->has_permission('Noticias.Content.Edit')) : ?>
+                            <td><?php echo anchor(SITE_AREA . '/content/noticias/edit/' . $record->idnoticias, '<i class="icon-pencil">&nbsp;</i>' . e(getTorneo($torneos,$record->idtorneo))) ?></td>
+                        <?php else: ?>
+                            <td><?php e(getTorneo($torneos,$record->idtorneo)) ?></td>
+                        <?php endif; ?>
+
+                        <td><?php e($record->titulo) ?></td>
+                        <td><?php //e($record->contenido)   ?></td>
+                        <td><?php e($record->fecha) ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else: ?>
+                <tr>
+                    <td colspan="5">No records found that match your selection.</td>
+                </tr>
+            <?php endif; ?>
+        </tbody>
+    </table>
+    <?php echo form_close(); ?>
 </div>
+
+<?php
+
+function getTorneo($torneos,$idtorneo) {
+    if (isset($torneos)) {
+        foreach ($torneos as $torneo) {
+            if ($torneo['id'] === $idtorneo)
+                return $torneo['nombre'];
+        }
+        return "None";
+    }else {
+        return $idtorneo;
+    }
+}
+?>
