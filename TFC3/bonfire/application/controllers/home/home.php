b794ab47ddb9f4624e1e0925ab6c5acb7e2e1602
hortelanobruno <hortelanobruno@bbc01b02-664b-0410-8048-2f08f105149e>
2014-04-02 22:31:17 +0000

    Noticias nuevo formato

--- TFC3/bonfire/application/controllers/home/home.php
+++ TFC3/bonfire/application/controllers/home/home.php
@@ -45,6 +45,7 @@ class Home extends Front_Controller {
      */
     public function index() {
         $data['torneos'] = $this->torneos_model->get_last_4_torneos();
+        $data['torneos_all'] = $this->torneos_model->get_all_torneos();
         $data['news'] = $this->noticias_model->get_last_10_news();
         $data['proximos_partidos'] = $this->torneos_model->get_proximos_partidos(5);
         $this->load->view('home/partes/header',$data);
--- TFC3/bonfire/application/logs/log-2014-04-02.php
+++ TFC3/bonfire/application/logs/log-2014-04-02.php
@@ -4,3 +4,5 @@ ERROR - 2014-04-02 23:42:15 --> Severity: Notice  --> Undefined property: stdCla
 ERROR - 2014-04-02 23:42:15 --> Severity: Notice  --> Undefined property: stdClass::$subtitulo C:\Users\Brunoli\Documents\NetBeansProjects\TFC3\bonfire\modules\noticias\views\content\index.php 46
 ERROR - 2014-04-02 23:43:16 --> Severity: Notice  --> Undefined property: stdClass::$subtitulo C:\Users\Brunoli\Documents\NetBeansProjects\TFC3\bonfire\modules\noticias\views\content\index.php 46
 ERROR - 2014-04-02 23:43:16 --> Severity: Notice  --> Undefined property: stdClass::$subtitulo C:\Users\Brunoli\Documents\NetBeansProjects\TFC3\bonfire\modules\noticias\views\content\index.php 46
+ERROR - 2014-04-02 23:58:36 --> 404 Page Not Found --> 
+ERROR - 2014-04-02 23:58:37 --> 404 Page Not Found --> 
